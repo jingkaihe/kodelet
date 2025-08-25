6ef18adf1e306807ba207cf5317e1c2b1f9e5689
Jingkai He <jingkai@hey.com>
2025-08-25 09:53:49 +0000

    feat(version): add build time to version information and update build scripts
    Co-authored-by: Kodelet <noreply@kodelet.com>
    Signed-off-by: Jingkai He <jingkai@hey.com>

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -13,12 +13,17 @@ var (
 	// GitCommit is the git commit SHA that was built
 	// This will be set during the build process
 	GitCommit = "unknown"
+
+	// BuildTime is the time when the binary was built
+	// This will be set during the build process
+	BuildTime = "unknown"
 )
 
 // Info represents version information
 type Info struct {
 	Version   string `json:"version"`
 	GitCommit string `json:"gitCommit"`
+	BuildTime string `json:"buildTime"`
 }
 
 // Get returns the version information
@@ -26,12 +31,13 @@ func Get() Info {
 	return Info{
 		Version:   Version,
 		GitCommit: GitCommit,
+		BuildTime: BuildTime,
 	}
 }
 
 // String returns the string representation of version info
 func (i Info) String() string {
-	return fmt.Sprintf("Version: %s, GitCommit: %s", i.Version, i.GitCommit)
+	return fmt.Sprintf("Version: %s, GitCommit: %s, BuildTime: %s", i.Version, i.GitCommit, i.BuildTime)
 }
 
 // JSON returns the JSON representation of version info
---
+++ pkg/version/version_test.go
@@ -0,0 +1,74 @@
+package version
+
+import (
+	"encoding/json"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGet(t *testing.T) {
+	info := Get()
+	
+	assert.Equal(t, Version, info.Version)
+	assert.Equal(t, GitCommit, info.GitCommit)
+	assert.Equal(t, BuildTime, info.BuildTime)
+}
+
+func TestInfo_String(t *testing.T) {
+	info := Info{
+		Version:   "1.0.0",
+		GitCommit: "abc123",
+		BuildTime: "Sun Aug 25 09:34:29 AM UTC 2025",
+	}
+	
+	result := info.String()
+	expected := "Version: 1.0.0, GitCommit: abc123, BuildTime: Sun Aug 25 09:34:29 AM UTC 2025"
+	assert.Equal(t, expected, result)
+}
+
+func TestInfo_JSON(t *testing.T) {
+	info := Info{
+		Version:   "1.0.0",
+		GitCommit: "abc123",
+		BuildTime: "Sun Aug 25 09:34:29 AM UTC 2025",
+	}
+	
+	jsonString, err := info.JSON()
+	require.NoError(t, err)
+	
+	// Verify it's valid JSON
+	var parsed Info
+	err = json.Unmarshal([]byte(jsonString), &parsed)
+	require.NoError(t, err)
+	
+	assert.Equal(t, info.Version, parsed.Version)
+	assert.Equal(t, info.GitCommit, parsed.GitCommit)
+	assert.Equal(t, info.BuildTime, parsed.BuildTime)
+	
+	// Verify all fields are present in JSON
+	assert.True(t, strings.Contains(jsonString, `"version"`))
+	assert.True(t, strings.Contains(jsonString, `"gitCommit"`))
+	assert.True(t, strings.Contains(jsonString, `"buildTime"`))
+}
+
+func TestInfo_JSONFormat(t *testing.T) {
+	info := Info{
+		Version:   "1.0.0",
+		GitCommit: "abc123",
+		BuildTime: "Sun Aug 25 09:34:29 AM UTC 2025",
+	}
+	
+	jsonString, err := info.JSON()
+	require.NoError(t, err)
+	
+	expectedJSON := `{
+  "version": "1.0.0",
+  "gitCommit": "abc123",
+  "buildTime": "Sun Aug 25 09:34:29 AM UTC 2025"
+}`
+	
+	assert.Equal(t, expectedJSON, jsonString)
+}
